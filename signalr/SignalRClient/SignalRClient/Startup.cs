5f4329e9a94d73f2c01ca85789081d7c0e17612e
magrande <mariano.grande@agilesight.com>
2021-10-12 17:42:37 -0300

    Refreshed signalID Reference implementation

--- signalr/SignalRClient/SignalRClient/Startup.cs
+++ signalr/SignalRClient/SignalRClient/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,8 +27,7 @@ namespace SignalRClient
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddSignalR();
         }
 
